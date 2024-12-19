8d7074188d54891d2027e77e0e4a64475b9af77f
schunsie <185822585+schunsie@users.noreply.github.com>
2024-12-19 20:50:21 +0100

    Add styling and HTML for UI (first draft)

--- script.js
+++ script.js
@@ -5,14 +5,25 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
-
-    this.info = function() {
-        return `${title} by ${author}, ${pages} pages, ${!read ? 'not read yet' : 'read'}`
-    }
 }
 
 function addBookToLibrary(title, author, pages, read) {
     let book = new Book(title, author, pages, read);
 
     myLibrary.push(book);
+}
+
+function displayLibrary() {
+    const tableBody = document.querySelector('tbody');
+    myLibrary.forEach((book) => {
+        const bookEntry = document.createElement('tr');
+        let stats = Object.values(book);
+        stats.forEach((stat) => {
+            let tableCell = document.createElement('td');
+            tableCell.textContent = stat;
+            bookEntry.appendChild(tableCell);
+        })
+
+        tableBody.appendChild(bookEntry);
+    })
 }
